f49482524548e6c67196b5c8d078d83079862d8e
remnantsofred <remnantsofred@gmail.com>
2022-12-07 10:50:42 -0800

    Cloned repo and reset up webpack. Added fonts

--- momos_misadventures/webpack.config.js
+++ momos_misadventures/webpack.config.js
@@ -42,10 +42,3 @@ module.exports = (env, argv) => {
   
   return config;
 }
-
-// const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-// module.exports = {
-// // Other rules like entry, output, devserver....,
-// plugins: [
-//     new NodePolyfillPlugin()
-// ]}
